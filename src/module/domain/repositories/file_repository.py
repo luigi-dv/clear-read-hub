4cb99184e08ff9bda4b022f934202fa90afe39cc
Luigi Davila <luigelo@ldvloper.com>
2024-02-17 08:53:16 +0100

    Added MongoDB FastAPI Support & OAuth2 FastAPI Support

--- src/module/domain/repositories/file_repository.py
+++ src/module/domain/repositories/file_repository.py
@@ -9,7 +9,7 @@ from azure.storage.blob import BlobClient
 """
     Domain Modules
 """
-from src.module.domain.entities.File import File
+from src.module.domain.entities.file import File
 
 
 class FileRepository(ABC):
